e65c3c6e0722c995c5951b2f77dbf9912d42ab17
Jace Babin <jbabin91@gmail.com>
2024-09-24 21:10:47 -0700

    feat: :sparkles: update vite config and add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 5173,
+  },
+  preview: {
+    port: 5173,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 });
